74098649e284cd29d498dcd14282f2d1198feecf
Peter Jipsen <jipsen@chapman.edu>
2025-01-06 16:45:15 -0800

    update

--- Test.lean
+++ Test.lean
@@ -1,1 +1,15 @@
---comment
+import Mathlib.Logic.Basic
+
+variable {S : Type*}
+
+variable [Mul S]
+
+variable (e₁ e₂ : S)
+
+/- Theorem 7: A binary system S =(S,*) can have at most one identity element -/
+
+theorem mul_one_identity : (∀ x : S, x * e₁ = x) ∧ (∀ x : S, e₂ * x = x) → e₁ = e₂ := by
+  intro h
+  have h1 := h.left e₂
+  have h2 := h.right e₁
+  rw [<-h1, h2]
