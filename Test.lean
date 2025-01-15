d099d114f798f8423f499c27dcd91f1ae523efca
Peter Jipsen <jipsen@chapman.edu>
2025-01-15 10:25:27 -0800

    u

--- Test.lean
+++ Test.lean
@@ -17,7 +17,10 @@ theorem mul_one_identity : (∀ x : S, x * e₁ = x) ∧ (∀ x : S, e₂ * x = 
 
 variable [Group G]
 
-/- Theorem 8: In a group G = (G,*) every element has a unique inverse. -/
+/- Theorem 8: In a group G = (G,*) every element has a unique inverse.
+
+In fact, we prove that if b is a left inverse and c is a right inverse of a,
+then b = c. -/
 
 theorem unique_inverse : ∀ a b c : G, b * a = 1 ∧ a * c = 1 → b = c := by
   intro a b c h
@@ -27,3 +30,28 @@ theorem unique_inverse : ∀ a b c : G, b * a = 1 ∧ a * c = 1 → b = c := by
     _ = (b * a) * c := by rw [mul_assoc]
     _ = 1 * c := by rw [h.1]
     _ = c := by rw [one_mul]
+
+/- Theorem 9: (i) x⁻¹⁻¹ = x -/
+
+theorem inverse_inverse : ∀ x : G, x⁻¹⁻¹ = x := by
+  intro x
+  calc
+    x⁻¹⁻¹ = x⁻¹⁻¹ * 1 := by rw [mul_one]
+    _ = x⁻¹⁻¹ * (x⁻¹ * x) := by rw [inv_mul_cancel]
+    _ = (x⁻¹⁻¹ * x⁻¹) * x := by rw [mul_assoc]
+    _ = 1 * x := by rw [inv_mul_cancel]
+    _ = x := by rw [one_mul]
+
+/- Theorem 9: (ii) (x * y)⁻¹ = y⁻¹ * x⁻¹ -/
+
+theorem mul_inverse : ∀ x y : G, (x * y)⁻¹ = y⁻¹ * x⁻¹ := by
+  intro x y
+  calc
+    (x * y)⁻¹ = (x * y)⁻¹ * 1 := by rw [mul_one]
+    _ = (x * y)⁻¹ * (x * x⁻¹) := by rw [mul_inv_cancel]
+    _ = (x * y)⁻¹ * ((x * 1) * x⁻¹) := by rw [mul_one]
+    _ = (x * y)⁻¹ * ((x * (y * y⁻¹)) * x⁻¹) := by rw [mul_inv_cancel]
+    _ = (x * y)⁻¹ * ((x * y) * (y⁻¹ * x⁻¹)) := by rw [mul_assoc, mul_assoc, mul_assoc]
+    _ = ((x * y)⁻¹ * (x * y)) * (y⁻¹ * x⁻¹) := by rw [←mul_assoc]
+    _ = 1 * (y⁻¹ * x⁻¹) := by rw [inv_mul_cancel]
+    _ = y⁻¹ * x⁻¹ := by rw [one_mul]
