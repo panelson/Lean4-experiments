14f479f51fa516bbbacdcd148b97588d699bdb3b
Peter Jipsen <jipsen@chapman.edu>
2025-01-31 08:23:41 -0800

    Peter updated permutations program

--- Hello.lean
+++ Hello.lean
@@ -1,12 +1,12 @@
 
 def permutations {X : Type} (l : List X) : List (List X) :=
+  let g p x := fun i => p.insertIdx i x
+  let h x xs := fun p => List.map (g p x) (List.range (xs.length + 1))
   match l with
   | [] => [[]]
-  | x :: xs =>
-    List.flatMap (permutations xs) fun perm =>
-      List.map (fun i => perm.insertIdx i x) (List.range (xs.length + 1))
+  | x :: xs => List.flatMap (permutations xs) (h x xs)
 
 def allPermutations (n : Nat) : List (List Nat) :=
   permutations (List.range n)
 
-def main : IO Unit := IO.println (allPermutations 11).length
+def main : IO Unit := IO.println (allPermutations 6)
