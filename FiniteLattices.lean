e90d33659b2325b57402d8838ddf85e8cc3088d2
Peter Jipsen <jipsen@chapman.edu>
2025-05-25 23:08:52 -0700

    upd

--- FiniteLattices.lean
+++ FiniteLattices.lean
@@ -1,5 +1,6 @@
 import Mathlib.Order.Lattice
 import Mathlib.Tactic.Tauto
+import Mathlib.Order.BooleanAlgebra
 
 /--
 C₂ denotes the set {0,1} viewed as a (complete) lattice, which in Lean is constructed
@@ -19,7 +20,7 @@ instance latticeC₂ : Lattice C₂ where
   sup_le       := by intro x y z; cases x <;> cases y <;> cases z <;> tauto
   inf_le_left  := by intro x y; cases x <;> cases y <;> tauto
   inf_le_right := by intro x y; cases x <;> cases y <;> tauto
-  le_inf       := by intro x y z; cases x <;> cases y <;> cases z <;> tauto
+  le_inf       := by intro x y z; cases x <;> cases y <;> cases z <;> simp
 
 inductive C₃ : Type where | a0 : C₃ | a1 : C₃ | a2 : C₃
 instance latticeC₃ : Lattice C₃ where
@@ -40,9 +41,42 @@ instance latticeC₃ : Lattice C₃ where
   sup_le       := by intro x y z; cases x <;> cases y <;> cases z <;> tauto
   inf_le_left  := by intro x y; cases x <;> cases y <;> tauto
   inf_le_right := by intro x y; cases x <;> cases y <;> tauto
-  le_inf       := by intro x y z; cases x <;> cases y <;> cases z <;> tauto
+  le_inf       := by intro x y z; cases x <;> cases y <;> cases z <;> simp
 
-#print Preorder
+set_option maxHeartbeats 300000
+
+inductive N₅ : Type where | z : N₅ | a : N₅ | b : N₅ | c : N₅ | t : N₅
+open N₅
+instance latticeN₅ : Lattice N₅ where
+  le  := fun
+    | a, z => False | a, b => False | b, z => False | b, a => False
+    | b, c => False | c, z => False | c, a => False | c, b => False
+    | t, z => False | t, a => False | t, b => False | t, c => False
+    | _, _ => True
+  sup := fun
+    | z, z => z | z, a => a | z, b => b | z, c => c
+    | a, z => a | a, a => a | a, b => t | a, c => c
+    | b, z => b | b, a => t | b, b => b | b, c => t
+    | c, z => c | c, a => c | c, b => t | c, c => c
+    | _, _ => t
+  inf := fun
+    | a, a => a | a, b => z | a, c => a | a, t => a
+    | b, a => z | b, b => b | b, c => z | b, t => b
+    | c, a => a | c, b => z | c, c => c | c, t => c
+    | t, a => a | t, b => b | t, c => c | t, t => t
+    | _, _ => z
+  le_refl      := by intro x; cases x <;> trivial
+  le_trans     := by intro x y z; cases x <;> cases y <;> cases z <;> simp
+  le_antisymm  := by intro x y; cases x <;> cases y <;> tauto
+  le_sup_left  := by intro x y; cases x <;> cases y <;> tauto
+  le_sup_right := by intro x y; cases x <;> cases y <;> tauto
+  sup_le       := by intro x y z; cases x <;> cases y <;> cases z <;> tauto
+  inf_le_left  := by intro x y; cases x <;> cases y <;> tauto
+  inf_le_right := by intro x y; cases x <;> cases y <;> tauto
+  le_inf       := by intro x y z; cases x <;> cases y <;> cases z <;> simp
+
+#print BooleanAlgebra
+#print latticeC₃
 
 instance preorder : Preorder C₂ where
   le := fun | C₂.a1, C₂.a0 => False | _, _ => True
